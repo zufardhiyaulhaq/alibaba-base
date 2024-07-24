3a4e684d3f8fe3ff8c973e9773244c06353210fd
Zufar Dhiyaullhaq <zufardhiyaulhaq@gmail.com>
2024-07-24 19:11:58 +0700

    feat: add output
    Signed-off-by: Zufar Dhiyaullhaq <zufardhiyaulhaq@gmail.com>

--- example/output.tf
+++ example/output.tf
@@ -1,64 +1,64 @@
 output "vpc_name" {
   description = "VPC name"
-  value       = alicloud_vpc.vpc.name
+  value       = module.alibaba_vpc.vpc_name
 }
 
 output "vpc_id" {
   description = "VPC id"
-  value       = alicloud_vpc.vpc.id
+  value       = module.alibaba_vpc.vpc_id
 }
 
 output "private_dns_name" {
   description = "private DNS name"
-  value       = alicloud_pvtz_zone.private_zone.zone_name
+  value       = module.alibaba_vpc.private_dns_name
 }
 
 output "utility_subnet_ids" {
   description = "Utility subnet IDs"
-  value       = values(alicloud_vswitch.utility_subnets).*.id
+  value       = module.alibaba_vpc.utility_subnet_ids
 }
 
 output "application_subnet_ids" {
   description = "Application subnet IDs"
-  value       = values(alicloud_vswitch.application_subnets).*.id
+  value       = module.alibaba_vpc.application_subnet_ids
 }
 
 output "stateful_subnet_ids" {
   description = "Stateful subnet IDs"
-  value       = values(alicloud_vswitch.stateful_subnets).*.id
+  value       = module.alibaba_vpc.stateful_subnet_ids
 }
 
 output "compliance_subnet_ids" {
   description = "Compliance subnet IDs"
-  value       = values(alicloud_vswitch.compliance_subnets).*.id
+  value       = module.alibaba_vpc.compliance_subnet_ids
 }
 
 output "public_subnet_ids" {
   description = "Public subnet IDs"
-  value       = values(alicloud_vswitch.public_subnets).*.id
+  value       = module.alibaba_vpc.public_subnet_ids
 }
 
 output "utility_subnets_cidr" {
   description = "Utility subnet CIDR"
-  value       = values(alicloud_vswitch.utility_subnets).*.cidr_block
+  value       = module.alibaba_vpc.utility_subnets_cidr
 }
 
 output "application_subnets_cidr" {
   description = "Application subnet CIDR"
-  value       = values(alicloud_vswitch.application_subnets).*.cidr_block
+  value       = module.alibaba_vpc.application_subnets_cidr
 }
 
 output "stateful_subnets_cidr" {
   description = "Stateful subnet CIDR"
-  value       = values(alicloud_vswitch.stateful_subnets).*.cidr_block
+  value       = module.alibaba_vpc.stateful_subnets_cidr
 }
 
 output "compliance_subnets_cidr" {
   description = "Compliance subnet CIDR"
-  value       = values(alicloud_vswitch.compliance_subnets).*.cidr_block
+  value       = module.alibaba_vpc.compliance_subnets_cidr
 }
 
 output "public_subnets_cidr" {
   description = "Public subnet CIDR"
-  value       = values(alicloud_vswitch.public_subnets).*.cidr_block
+  value       = module.alibaba_vpc.public_subnets_cidr
 }
