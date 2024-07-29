32738c03dfadcd90b3d7c75559fa81a9cdadf7fe
Zufar Dhiyaullhaq <zufardhiyaulhaq@gmail.com>
2024-07-29 20:42:35 +0700

    feat: add network acl
    Signed-off-by: Zufar Dhiyaullhaq <zufardhiyaulhaq@gmail.com>

--- example/main.tf
+++ example/main.tf
@@ -1,115 +1,115 @@
 module "alibaba_vpc" {
   source = "../"
 
-  vpc_name = "coins-vpc"
-  vpc_cidr = "10.30.0.0/16"
+  vpc_name = "app-vpc"
+  vpc_cidr = "10.31.0.0/16"
 
   label_environment = "staging"
   label_team        = "coins"
 
   public_subnets = {
     "public-vm-a" = {
-      cidr              = "10.30.78.0/24",
+      cidr              = "10.31.78.0/24",
       availability_zone = "ap-southeast-5a",
     },
     "public-vm-b" = {
-      cidr              = "10.30.79.0/24",
+      cidr              = "10.31.79.0/24",
       availability_zone = "ap-southeast-5b",
     },
     "public-vm-c" = {
-      cidr              = "10.30.80.0/24",
+      cidr              = "10.31.80.0/24",
       availability_zone = "ap-southeast-5c",
     },
     "public-k8s-a" = {
-      cidr              = "10.30.66.0/23",
+      cidr              = "10.31.66.0/23",
       availability_zone = "ap-southeast-5a",
     },
     "public-k8s-b" = {
-      cidr              = "10.30.68.0/23",
+      cidr              = "10.31.68.0/23",
       availability_zone = "ap-southeast-5b",
     },
     "public-k8s-c" = {
-      cidr              = "10.30.70.0/23",
+      cidr              = "10.31.70.0/23",
       availability_zone = "ap-southeast-5c",
     },
   }
   utility_subnets = {
     "utility-vm-a" = {
-      cidr              = "10.30.75.0/24",
+      cidr              = "10.31.75.0/24",
       availability_zone = "ap-southeast-5a",
     },
     "utility-vm-b" = {
-      cidr              = "10.30.76.0/24",
+      cidr              = "10.31.76.0/24",
       availability_zone = "ap-southeast-5b",
     },
     "utility-vm-c" = {
-      cidr              = "10.30.77.0/24",
+      cidr              = "10.31.77.0/24",
       availability_zone = "ap-southeast-5c",
     },
     "utility-k8s-a" = {
-      cidr              = "10.30.60.0/23",
+      cidr              = "10.31.60.0/23",
       availability_zone = "ap-southeast-5a",
     },
     "utility-k8s-b" = {
-      cidr              = "10.30.62.0/23",
+      cidr              = "10.31.62.0/23",
       availability_zone = "ap-southeast-5b",
     },
     "utility-k8s-c" = {
-      cidr              = "10.30.64.0/23",
+      cidr              = "10.31.64.0/23",
       availability_zone = "ap-southeast-5c",
     },
   }
   application_subnets = {
     "application-vm-a" = {
-      cidr              = "10.30.72.0/24",
+      cidr              = "10.31.72.0/24",
       availability_zone = "ap-southeast-5a",
     },
     "application-vm-b" = {
-      cidr              = "10.30.73.0/24",
+      cidr              = "10.31.73.0/24",
       availability_zone = "ap-southeast-5b",
     },
     "application-vm-c" = {
-      cidr              = "10.30.74.0/24",
+      cidr              = "10.31.74.0/24",
       availability_zone = "ap-southeast-5c",
     },
     "application-k8s-a" = {
-      cidr              = "10.30.0.0/20",
+      cidr              = "10.31.0.0/20",
       availability_zone = "ap-southeast-5a",
     },
     "application-k8s-b" = {
-      cidr              = "10.30.16.0/20",
+      cidr              = "10.31.16.0/20",
       availability_zone = "ap-southeast-5b",
     },
     "application-k8s-c" = {
-      cidr              = "10.30.32.0/20",
+      cidr              = "10.31.32.0/20",
       availability_zone = "ap-southeast-5c",
     },
   }
   stateful_subnets = {
     "stateful-vm-a" = {
-      cidr              = "10.30.48.0/22",
+      cidr              = "10.31.48.0/22",
       availability_zone = "ap-southeast-5a",
     },
     "stateful-vm-b" = {
-      cidr              = "10.30.52.0/22",
+      cidr              = "10.31.52.0/22",
       availability_zone = "ap-southeast-5b",
     },
     "stateful-vm-c" = {
-      cidr              = "10.30.56.0/22",
+      cidr              = "10.31.56.0/22",
       availability_zone = "ap-southeast-5c",
     },
   }
   compliance_subnets = {
     "compliance-vm-a" = {
-      cidr              = "10.30.81.0/24",
+      cidr              = "10.31.81.0/24",
       availability_zone = "ap-southeast-5a",
     },
     "compliance-vm-b" = {
-      cidr              = "10.30.82.0/24",
+      cidr              = "10.31.82.0/24",
       availability_zone = "ap-southeast-5b",
     },
     "compliance-vm-c" = {
-      cidr              = "10.30.84.0/24",
+      cidr              = "10.31.84.0/24",
       availability_zone = "ap-southeast-5c",
     },
   }
@@ -131,5 +131,70 @@ module "alibaba_vpc" {
     "application-nat-ip-06" = {},
     "application-nat-ip-07" = {},
     "application-nat-ip-08" = {},
+  }
+}
+
+module "alibaba_network_acl" {
+  source = "github.com/zufardhiyaulhaq/alibaba-network-acl"
+
+  vpc_id            = module.alibaba_vpc.vpc_id
+  label_environment = "staging"
+  label_team        = "coins"
+
+  public_subnets_cidr      = module.alibaba_vpc.public_subnets_cidr
+  utility_subnets_cidr     = module.alibaba_vpc.utility_subnets_cidr
+  application_subnets_cidr = module.alibaba_vpc.application_subnets_cidr
+  stateful_subnets_cidr    = module.alibaba_vpc.stateful_subnets_cidr
+  compliance_subnets_cidr  = module.alibaba_vpc.compliance_subnets_cidr
+
+  public_subnets_id      = module.alibaba_vpc.public_subnet_ids
+  utility_subnets_id     = module.alibaba_vpc.utility_subnet_ids
+  application_subnets_id = module.alibaba_vpc.application_subnet_ids
+  stateful_subnets_id    = module.alibaba_vpc.stateful_subnet_ids
+  compliance_subnets_id  = module.alibaba_vpc.compliance_subnet_ids
+
+  additional_ingress_application_rules = {
+    "temp-allow-6443-from-public" = {
+      policy = "accept"
+      protocol = "tcp"
+      port = "6443"
+      source_cidr_ip = "0.0.0.0/0"
+    }
+  }
+
+  additional_ingress_utility_rules = {
+    "temp-allow-6443-from-public" = {
+      policy = "accept"
+      protocol = "tcp"
+      port = "6443"
+      source_cidr_ip = "0.0.0.0/0"
+    }
+  }
+
+  additional_ingress_public_rules = {
+    "temp-allow-6443-from-public" = {
+      policy = "accept"
+      protocol = "tcp"
+      port = "6443"
+      source_cidr_ip = "0.0.0.0/0"
+    }
+  }
+
+  additional_ingress_stateful_rules = {
+    "temp-allow-6443-from-public" = {
+      policy = "accept"
+      protocol = "tcp"
+      port = "6443"
+      source_cidr_ip = "0.0.0.0/0"
+    }
+  }
+
+  additional_ingress_compliance_rules = {
+    "temp-allow-6443-from-public" = {
+      policy = "accept"
+      protocol = "tcp"
+      port = "6443"
+      source_cidr_ip = "0.0.0.0/0"
+    }
   }
 }
